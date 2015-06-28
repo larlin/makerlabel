26370caf911b2fda55fc6b30e584d9038e80e9f3
Lars Lindholm <larlin@lysator.liu.se>
2015-06-28 15:14:15 +0200

    Moved tags app urls to root of site. Now compatiable with urls generated for the qr-code. Short saves qr-code size.

--- tagdatabase/tagdatabase/urls.py
+++ tagdatabase/tagdatabase/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tagdatabase.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tags/', include('tags.urls', namespace="tags")),
+    url(r'^', include('tags.urls', namespace="tags")),
 )
